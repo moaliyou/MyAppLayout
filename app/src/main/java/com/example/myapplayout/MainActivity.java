56b3c01c2db2f63fdb3155826dd4a4003a5a22ad
moaliyou <moaliyou47@gmail.com>
2022-07-29 16:03:40 +0300

    Create & Modify: new XML files and modified Main.java File

--- app/src/main/java/com/example/myapplayout/MainActivity.java
+++ app/src/main/java/com/example/myapplayout/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.myapplayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.questions);
+        setContentView(R.layout.simple_registration);
+
+//        TextView textQuestion = findViewById(R.id.textQuestions);
+//        Button buttonNext = findViewById(R.id.buttonNext);
+//
+//        buttonNext.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                textQuestion.setText("What is the capital city of Somalia?");
+//            }
+//        });
+
     }
+
+    public void onClick(View v){
+        EditText editTextFirstName = findViewById(R.id.editTextFirstName);
+        EditText editTextLastName = findViewById(R.id.editTextKastName);
+        EditText editTextEmail = findViewById(R.id.editTextEmail);
+
+        TextView textViewFirstName = findViewById(R.id.textFirstName);
+        TextView textViewLastName = findViewById(R.id.textLastName);
+        TextView textViewEmail = findViewById(R.id.textEmail);
+
+        textViewFirstName.setText("First Name: " + editTextFirstName.getText());
+        textViewLastName.setText("Last Name: " + editTextLastName.getText());
+        textViewEmail.setText("Email: " + editTextEmail.getText());
+
+    }
+
 }
